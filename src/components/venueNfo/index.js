9a2e68177e4ca79c55a1a11f32c3607fead6ddbb
ahmed-humayun <93235542+ahmed-humayun@users.noreply.github.com>
2022-01-26 22:00:09 -0500

    fixed venueNfo

--- src/components/venueNfo/index.js
+++ src/components/venueNfo/index.js
@@ -1,18 +1,18 @@
 import React from 'react';
 import Zoom from 'react-reveal/Zoom'; // Content should go here.
 // the import below is to get the named images from their corresponding folder.
-import brad from '../../resources/images/Players/brad.jpg' 
-import chloe from '../../resources/images/Players/chloe.jpg'
-import hanyu from '../../resources/images/Players/hanyu.jpg'
-import mikaela from '../../resources/images/Players/mikaela.jpg'
-import suzanne from '../../resources/images/Players/suzanne.jpg'
+import brad from '../../resources/images/players/brad.jpg' 
+import chloe from '../../resources/images/players/chloe.jpg'
+import hanyu from '../../resources/images/players/hanyu.jpg'
+import mikaela from '../../resources/images/players/mikaela.jpg'
+import suzanne from '../../resources/images/players/suzanne.jpg'
 const VunueNfo = () => {
     return (
         <div className="bck_black">
             <h1>SOME OF THE PLAYERS</h1>
-            <div className="center_wrapper"> // to put the header at the center.
+            <div className="center_wrapper"> 
                 <div className="vn_wrapper">
-                    <Zoom duration={500}>  //zooming animation that reveals text inside the tag
+                    <Zoom duration={500}>  
                         <div className="vn_item">
                             <div className="vn_outer"> 
                                 <div className="vn_inner">
@@ -21,17 +21,15 @@ const VunueNfo = () => {
                                         style={{width:"100%", // width size
                                         height: "auto", // height size
                                         textAlign: "center"}}/> 
-                                    <div className="vn_title"> // title of the picture.
-                                        Brad Gushue 
+                                    <div className="vn_title"> 
                                     </div>
-                                    <div className="vn_desc"> // to add a description 
-                                        Curling Player
+                                    <div className="vn_desc"> 
                                     </div>
                                 </div>
                             </div>
                         </div>
                     </Zoom>
-                    <Zoom duration={500} delay={500}> //zooming animation that reveals text inside the tag
+                    <Zoom duration={500} delay={500}> 
                         <div className="vn_item">
                             <div className="vn_outer">
                                 <div className="vn_inner">
@@ -40,17 +38,17 @@ const VunueNfo = () => {
                                         style={{width:"100%", // width size
                                         height: "auto", // height size
                                         textAlign: "center"}}/>
-                                    <div className="vn_title"> // title of the picture.
+                                    <div className="vn_title"> 
                                         Chloe Kim
                                     </div>
-                                    <div className="vn_desc"> // to add a description 
+                                    <div className="vn_desc"> 
                                        Snowboarder
                                     </div>
                                 </div>
                             </div>
                         </div>
                     </Zoom>
-                    <Zoom duration={500}> //zooming animation that reveals text inside the tag
+                    <Zoom duration={500}> 
                         <div className="vn_item">
                             <div className="vn_outer">
                                 <div className="vn_inner">
@@ -59,17 +57,17 @@ const VunueNfo = () => {
                                         style={{width:"100%", // width size of the picture
                                         height: "auto", // height size of the picture // textalign is to make the text be centered.
                                         textAlign: "center"}}/>
-                                    <div className="vn_title"> // title of the picture.
+                                    <div className="vn_title"> 
                                     Hanyu Yuzuru
                                     </div>
-                                    <div className="vn_desc"> // to add a description .
+                                    <div className="vn_desc"> 
                                         Skater
                                     </div>
                                 </div>
                             </div>
                         </div>
                     </Zoom>
-                    <Zoom duration={500}> //zooming animation that reveals text inside the tag
+                    <Zoom duration={500}> 
                         <div className="vn_item">
                             <div className="vn_outer">
                                 <div className="vn_inner">
@@ -78,17 +76,17 @@ const VunueNfo = () => {
                                         style={{width:"100%", // width size of the picture
                                         height: "auto", // height size of the picture // textalign is to make the text be centered.
                                         textAlign: "center"}}/>          
-                                    <div className="vn_title"> // title of the picture.
+                                    <div className="vn_title"> 
                                     Mikaela Shiffrin
                                     </div>
-                                    <div className="vn_desc"> // to add a description .
+                                    <div className="vn_desc"> 
                                         Alpine Skier
                                     </div>
                                 </div>
                             </div>
                         </div>
                         </Zoom>
-                        <Zoom duration={500}> //zooming animation that reveals text inside the tag
+                        <Zoom duration={500}> 
                         <div className="vn_item">
                             <div className="vn_outer">
                                 <div className="vn_inner">
@@ -97,10 +95,10 @@ const VunueNfo = () => {
                                         style={{width:"100%", // width size of the picture
                                         height: "auto", // height size of the picture // textalign is to make the text be centered.
                                         textAlign: "center"}}/> 
-                                    <div className="vn_title"> // title of the picture.
+                                    <div className="vn_title"> 
                                     suzanne Schulting
                                     </div>
-                                    <div className="vn_desc"> // to add a description .
+                                    <div className="vn_desc"> 
                                         Speed Skater
                                     </div>
                                 </div>
